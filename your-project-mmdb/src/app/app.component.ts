4c6a33a57099b658a230f653259ed5db5eabe029
Narkhedepratik6125 <118867323+Narkhedepratik6125@users.noreply.github.com>
2024-01-24 20:47:29 +0530

    new add
    new add

--- your-project-mmdb/src/app/app.component.ts
+++ your-project-mmdb/src/app/app.component.ts
@@ -1,43 +1,55 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpserviceService } from './httpservice.service';
-import { Subscription, debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import {
+  Subscription,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+} from 'rxjs';
 import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'your-project-mmdb';   
+  title = 'your-project-mmdb';
 
-  skeletonArr = Array(5)
+  skeletonArr = Array(5);
   keyupSubs!: Subscription;
-    
-    constructor(private httpservice :HttpserviceService) {
-    
-    }
+  searchResults: any[] = [];
+  notFound = false;
 
+  constructor(private httpservice: HttpserviceService) {}
 
-    ngOnInit(): void {
-
-      this.keyupSubs = this.httpservice.keyupSubject
+  ngOnInit(): void {
+    this.keyupSubs = this.httpservice.keyupSubject
       .pipe(
-        map((e:any)=> e.target.value),
+        map((e: any) => e.target.value),
         filter(Boolean),
         debounceTime(500),
         distinctUntilChanged(),
         switchMap((search) => this.httpservice.fetchMovies(search))
       )
-      .subscribe(res=> 
-        { 
-          console.log(res);
-        });
-      
-    } 
-    
-    ngOnDestroy(): void {
-      if(this.keyupSubs) this.keyupSubs.unsubscribe()
-    }
+      .subscribe({
+        next: (response: any) => {
+          this.notFound = false;
+          if (response?.results.length) {
+
+            this.searchResults = response.results;
+            
+            } else {
+            
+            this.notFound = true;
+          }
+        },
+        error: (err) => {},
+      });
   }
 
+  ngOnDestroy(): void {
+    if (this.keyupSubs) this.keyupSubs.unsubscribe();
+  }
+}
